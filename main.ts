6f9c04c9b91a784c74511d6261c24a3c6ac5de79
bowiei <info@peanutkingsolution.com>
2025-07-24 19:04:53 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1313,7 +1313,7 @@ namespace pksdriver {
             return yaw;
         } else {
             // Apply Kalman filter
-            kalmanFilterYaw();
+            return kalmanFilterYaw();
         }
     }
 
